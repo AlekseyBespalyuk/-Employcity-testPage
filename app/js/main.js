09c5d64ca2c54530e26c2586231f7475245de155
AlekseyBespalyuk <alekseibespalyuk@yandex.ru>
2021-11-21 23:21:20 +0300

    add

---
+++ app/js/main.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready((function(){$(".search__btn").click((function(){$(this).hasClass("active-btn")?$(".search__input").removeClass("active").addClass("re-active").val(""):$(".search__input").removeClass("re-active").addClass("active").focus(),$(this).toggleClass("active-btn")}))}));
